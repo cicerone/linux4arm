fa2e8410b009703497b9b10ab11d7b100db553b3
roni <cicerone.mihalache@kotys.biz>
2013-12-03 17:20:06 -0800

    Using the right dir.

--- webcam/send_video.sh
+++ webcam/send_video.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
+#!/bin/bash -x
 while true; do
     rm -f final_loop.sh
     rm -f email.log
     rm -f info.log
-    curl -u testfswebcam:asdfasdfasdf --silent "https://mail.google.com/mail/feed/atom" | grep summary > email.log
+    curl -u roni:roni --silent http://mailcam.co/mail_cam/d$CAM_ID/info.log | grep summary > email.log
     head -n 1 email.log > info.log
     awk -f build_fswebcam.awk info.log
-    chmod 777 final_loop.sh
+    chmod 700 final_loop.sh
     ./final_loop.sh
-    mpack -s video movie_1.mp4 videos4linux@gmail.com
+#FIXME add the right email
+#    mpack -s video movie_1.mp4 cicerone.mihalache@gmail.com
     sleep 10 
 done
