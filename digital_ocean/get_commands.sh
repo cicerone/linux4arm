a6de37b88635c4ec5ce1a44b5cde3f4c86f72d4f
roni <cicerone.mihalache@kotys.biz>
2013-12-09 19:23:10 -0800

    Using the mailcam.co email.

--- digital_ocean/get_commands.sh
+++ digital_ocean/get_commands.sh
@@ -1,19 +1,30 @@
 #!/bin/bash -x
+
+NEW_EMAIL_DIR="/home/roni/Maildir/new"
+OLD_EMAIL_DIR="/home/roni/Maildir/cur"
+MAILCAM_DIR="/home/roni/mail_cam"
+
 while true; do
-    rm -f email.log
-    rm -f info.log
-    curl -u testfswebcam:asdfasdfasdf --silent "https://mail.google.com/mail/feed/atom" | grep summary > email_new.log
-    diff email_new.log email_old.log | tail -n +2  > email.log
-    ./check_commands.py
-    ./copy_commands.py
 
-#   sleep 1
-#   if [ -s info.log ]; then
-#       cp -f info.log /var/www/mailcam.co/public_html/mail_cam/d000000
-#   fi
-    cp -f email_new.log email_old.log
-    echo "info.log is ready!"
-#    head -n 1 email.log > info.log
+# look for empty dir 
+    if [ "$(ls -A $NEW_EMAIL_DIR)" ]; then
+        echo "Take action $NEW_EMAIL_DIR is not Empty"
+        sleep 1
+        rm -f $MAILCAM_DIR/email.log
+        rm -f $MAILCAM_DIR/info.log
+        last_file=$(ls -t $NEW_EMAIL_DIR| tail -1)
+        sed '/Start\|Stop/!d' $NEW_EMAIL_DIR/$last_file > $MAILCAM_DIR/email.log
 
-    sleep 10 
+        ./check_commands.py
+        ./copy_commands.py
+        mv  $NEW_EMAIL_DIR/$last_file $OLD_EMAIL_DIR/$last_file 
+        echo "info.log is ready!"
+    else
+        echo "$NEW_EMAIL_DIR is Empty"
+    fi
+
+    sleep 1 
 done
+
+
+
