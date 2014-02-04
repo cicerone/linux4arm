0b6c23b4513cecbe6326a9bd882897f55edb73d9
roni <cicerone.mihalache@kotys.biz>
2014-02-03 20:37:12 -0800

    Generate the file that shows the new params.

--- digital_ocean/copy_commands.py
+++ digital_ocean/copy_commands.py
@@ -11,15 +11,20 @@ f = open('commands.log')
 cmd_lines = f.readlines()
 f.close()
 
-EMAIL_ID_INDEX = 2
-EMAIL_PSWD_INDEX = 4
+USER_NAME_ID  = 2
+MODE_ID       = 6
+PIC_NR_ID     = 14
+DELAY_ID      = 16
+MOVIE_TIME_ID = 18
+HEART_BEAT_ID = 22
+ACTION_ID     = 26
 
 for cmd_line in cmd_lines:
 #    print "line number %d", i, email_lines[i]
     cmd_fields = cmd_line.split(":")
-   # print "cmd field 3 and 5", cmd_fields[EMAIL_ID_INDEX], email_fields[EMAIL_PSWD_INDEX]
+   # print "cmd field 3 and 5", cmd_fields[USER_NAME_ID], email_fields[EMAIL_PSWD_INDEX]
     dir2copy = "/home/"
-    dir2copy += cmd_fields[EMAIL_ID_INDEX]
+    dir2copy += cmd_fields[USER_NAME_ID]
     if os.path.isdir(dir2copy) == False:
         os.makedirs(dir2copy)
 
@@ -32,5 +37,16 @@ for cmd_line in cmd_lines:
     if os.path.isfile(file2remove):
         os.remove(file2remove)
     shutil.move("info.log", dir2copy)
+
+    f = open('new_command.txt', "w")
+    seen_cmd = "Mode = " + cmd_fields[MODE_ID] 
+    seen_cmd = seen_cmd + "\nPictures Nr = " + cmd_fields[PIC_NR_ID] 
+    seen_cmd = seen_cmd +  "\nDelay = " + cmd_fields[DELAY_ID]
+    seen_cmd = seen_cmd +  "\nMovie Duration = " + cmd_fields[MOVIE_TIME_ID]
+    seen_cmd = seen_cmd +  "\nHeartBeat = " + cmd_fields[HEART_BEAT_ID]
+    seen_cmd = seen_cmd +  "\nAction = " + cmd_fields[ACTION_ID]
+    f.write(seen_cmd)
+    f.close()
+    shutil.move("new_command.txt", dir2copy)
 
 
